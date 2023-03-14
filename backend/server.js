03bc60ca2d75e55e0b119d844fd91f10df73a848
JoseCoelho25 <zenani87@hotmail.com>
2023-03-14 12:47:26 +0000

    initial setup

---
+++ backend/server.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const errorHandler = require('./middleware/error');
+const fileupload = require('express-fileupload');
+const connectDB = require('./config/db');
+const colors = require('colors');
+
+
+// Route files
+
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+connectDB();
+
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+// File uploading
+app.use(fileupload());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Mount routers
+
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+PORT,
+console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+);
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+server.close(() => process.exit(1));
+});
