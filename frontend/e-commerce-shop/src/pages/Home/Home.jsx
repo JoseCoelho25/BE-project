151e942c985f4bf3c1e752b36a07cb0209435cb6
JoseCoelho25 <zenani87@hotmail.com>
2023-03-15 15:28:03 +0000

    displaying products in Home page

--- frontend/e-commerce-shop/src/pages/Home/Home.jsx
+++ frontend/e-commerce-shop/src/pages/Home/Home.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchAllProducts } from '../../slices/shopSlice';
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.shop.products);
+  const status = useSelector((state) => state.shop.status);
+  const error = useSelector((state) => state.shop.error);
+
+  useEffect(() => {
+    dispatch(fetchAllProducts());
+  }, [dispatch]);
+
+  if (status === 'loading') {
+    return <div>Loading...</div>;
+  }
+
+  if (status === 'failed') {
+    return <div>{error}</div>;
+  }
+
   return (
-    <div>Home</div>
+    <div>
+      {products.map((product) => (
+        <div key={product._id}>
+          <h2>{product.title}</h2>
+          <p>{product.price}</p>
+          <img src={product.imageUrl} alt="clothes imgs" srcset="" />
+          <p>{product.description}</p>
+          </div>))}
+          </div>
   )
 }
 
