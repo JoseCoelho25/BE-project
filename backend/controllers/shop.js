1a32c21bcd04f82dc7816d0481f728229803218f
JoseCoelho25 <zenani87@hotmail.com>
2023-03-15 15:27:13 +0000

    setting controller to get all products in DB

--- backend/controllers/shop.js
+++ backend/controllers/shop.js
@@ -4,7 +4,8 @@ const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 
 exports.getProducts = asyncHandler(async(req,res,next) => {
-    res.send('get products works')
+    const products = await Product.find();
+    res.status(200).json({success:true, count:products.length, data:products})
 })
 
 exports.getProduct = asyncHandler(async(req,res,next) => {
