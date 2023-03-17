c2785440bf4113c5aa2245e1bd17f91de836f245
JoseCoelho25 <zenani87@hotmail.com>
2023-03-17 23:37:19 +0000

    implemented remove item from cart function in FE

--- frontend/e-commerce-shop/src/pages/Cart/Cart.jsx
+++ frontend/e-commerce-shop/src/pages/Cart/Cart.jsx
@@ -7,6 +7,27 @@ const Cart = () => {
   const userToken = useSelector((state) => state.auth.user);
   //console.log(userToken.token)
 
+  const handleRemoveItem = async (productId) => {
+    try {
+      const response = await fetch(api + '/cart', {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${userToken.token}`,
+        },
+        body: JSON.stringify({
+          productId: productId,
+        }),
+      });
+      const data = await response.json();
+      console.log(data);
+      // update the cart state after removing the item
+      setCart(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   useEffect(() => {
     const fetchCart = async () => {
       try {
@@ -34,6 +55,7 @@ const Cart = () => {
           <img src={product.product.imageUrl} alt="cart-img" className='h-72' />
           <p className='mx-2 text-xl font-bold mt-4'>{product.quantity}</p>
           <p className='text-xl mt-4'>{product.product.title}</p>
+          <button className='mt-20 border w-full border-black p-2 bg' onClick={()=> handleRemoveItem(product.productId)}>Remove item from cart</button>
         </div>
       ))}
     </div>
