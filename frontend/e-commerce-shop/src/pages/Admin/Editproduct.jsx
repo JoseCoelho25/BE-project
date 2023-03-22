cbc00bf29154ec22ab58867aeac4e005b65b0c71
JoseCoelho25 <zenani87@hotmail.com>
2023-03-22 15:17:29 +0000

    get edit product form in FE

--- frontend/e-commerce-shop/src/pages/Admin/Editproduct.jsx
+++ frontend/e-commerce-shop/src/pages/Admin/Editproduct.jsx
@@ -4,15 +4,26 @@ import { api } from "../../utils/config";
 
 const Editproduct = () => {
   const { productId } = useParams();
-  const [product, setProduct] = useState({});
-
+  const [product, setProduct] = useState({
+    category: [],
+    description: '',
+    imageUrl: '',
+    price: 0,
+    title: '',
+  });
 
   useEffect(() => {
     const fetchProduct = async () => {
       try {
-        const response = await fetch(api + `/edit-products/${productId}`);
+        const response = await fetch(api + `/admin/edit-products/${productId}`,{
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          credentials: 'include',
+        });
         const data = await response.json();
-        setProduct(data);
+        setProduct(data.data);
       } catch (err) {
         console.log(err);
       }
@@ -21,10 +32,78 @@ const Editproduct = () => {
     fetchProduct();
   }, [productId]);
 
-  console.log(product)
-  return (
-    <div>Edit-product</div>
-  )
-}
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setProduct((prevProduct) => ({
+      ...prevProduct,
+      [name]: value,
+    }));
+  };
 
-export default Editproduct
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Add code to submit form data to API
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className='mx-auto w-2/3'>
+      <div className='flex'>
+        <label htmlFor="title">Title</label>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          value={product.title}
+          onChange={handleChange}
+          className='ml-4 w-full'
+        />
+      </div>
+      <div className='flex'>
+        <label htmlFor="description">Description</label>
+        <textarea
+          id="description"
+          name="description"
+          value={product.description}
+          onChange={handleChange}
+          className='ml-4 w-full'
+        />
+      </div>
+      <div>
+        <label htmlFor="imageUrl">Image URL</label>
+        <input
+          type="file"
+          id="imageUrl"
+          name="imageUrl"
+          className='ml-4'
+          // value={product.imageUrl}
+          // onChange={handleChange}
+        />
+      </div>
+      <div className='flex'>
+        <label htmlFor="price">Price</label>
+        <input
+          type="number"
+          id="price"
+          name="price"
+          value={product.price}
+          onChange={handleChange}
+          className='ml-4 w-full'
+        />
+      </div>
+      <div className='flex'>
+        <label htmlFor="category">Category</label>
+        <input
+          type='text'
+          id="category"
+          name="category"
+          value={product.category}
+          onChange={handleChange}
+          className='ml-4 w-full'
+        />
+      </div>
+      <button type="submit">Save</button>
+    </form>
+  );
+};
+
+export default Editproduct;
