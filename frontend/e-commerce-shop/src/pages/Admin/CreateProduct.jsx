cab094fb1d8b7fb219e1916584027cc1d35a9611
JoseCoelho25 <zenani87@hotmail.com>
2023-03-23 22:41:53 +0000

    added functionality to remove prod by admin in FE

--- frontend/e-commerce-shop/src/pages/Admin/CreateProduct.jsx
+++ frontend/e-commerce-shop/src/pages/Admin/CreateProduct.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import { api } from "../../utils/config";
+import { useNavigate } from 'react-router-dom';
 
 const CreateProduct = () => {
     const [image, setImage] = useState(null);
@@ -9,106 +10,103 @@ const CreateProduct = () => {
         imageUrl: '',
         price: 0,
         title: '',
-      });
-      
+        });
+
+    const navigate = useNavigate()
 
     const handleSubmit = async(event) => {
         event.preventDefault();
         try {
-          const formData = new FormData();
-          formData.append('image', image);
-          Object.entries(product).forEach(([key, value]) => formData.append(key, value));
+            const formData = new FormData();
+            formData.append('image', image);
+            Object.entries(product).forEach(([key, value]) => formData.append(key, value));
     
         const response = await fetch(api + '/admin/add-product', {
-          method: 'POST',
-          headers: {
+            method: 'POST',
+            headers: {
             Accept: 'application/json',
-          },
-          body: formData,
-          credentials: 'include',
+            },
+            body: formData,
+            credentials: 'include',
         });
     
-          const data = await response.json();
-          console.log(data);
+            const data = await response.json();
+            console.log(data);
+            navigate('/products')
         } catch (err) {
-          console.log(err);
+            console.log(err);
         }
-      };
+    };
     
 
-      const handleChange = (event) => {
+    const handleChange = (event) => {
         const { name, value } = event.target;
         setProduct((prevProduct) => ({
-          ...prevProduct,
-          [name]: name === 'image' ? prevProduct.image : value,
+        ...prevProduct,
+        [name]: name === 'image' ? prevProduct.image : value,
         }));
-      };
-      
-//  console.log(image)
-   console.log(product)
-  return (
+    };  
+
+return (
     <div>
         <form onSubmit={handleSubmit} className='mx-auto w-2/3'>
-      <div className='flex mb-2'>
+    <div className='flex mb-2'>
         <label htmlFor="title">Title</label>
         <input
-          type="text"
-          id="title"
-          name="title"
-          value={product.title}
-          onChange={handleChange}
-          className='ml-20 w-full border border-black'
+            type="text"
+            id="title"
+            name="title"
+            value={product.title}
+            onChange={handleChange}
+            className='ml-20 w-full border border-black'
         />
-      </div>
-      <div className='flex mb-2'>
+    </div>
+    <div className='flex mb-2'>
         <label htmlFor="description">Description</label>
         <textarea
-          id="description"
-          name="description"
-          value={product.description}
-          onChange={handleChange}
-          className='ml-7 w-full border border-black'
+            id="description"
+            name="description"
+            value={product.description}
+            onChange={handleChange}
+            className='ml-7 w-full border border-black'
         />
-      </div>
-      <div>
+    </div>
+    <div>
         <label htmlFor="image">Image</label>
         <input 
-          type="file" 
-          id="image" 
-          name="image" 
-          onChange={(e) => setImage(e.target.files[0])} 
-          className='ml-16 mb-2'
+            type="file" 
+            id="image" 
+            name="image" 
+            onChange={(e) => setImage(e.target.files[0])} 
+            className='ml-16 mb-2'
         />
-      </div>
-      <div className='flex mb-2 gap-x-3'>
+    </div>
+    <div className='flex mb-2 gap-x-3'>
         <label htmlFor="price">Price</label>
         <input
-          type="number"
-          id="price"
-          name="price"
-          value={product.price}
-          onChange={handleChange}
-          className='ml-16 w-full border border-black'
+            type="number"
+            id="price"
+            name="price"
+            value={product.price}
+            onChange={handleChange}
+            className='ml-16 w-full border border-black'
         />
-      </div>
-      <div className='flex'>
+    </div>
+    <div className='flex'>
         <label htmlFor="category">Category</label>
         <input
-          type='text'
-          id="category"
-          name="category"
-          value={product.category}
-          onChange={handleChange}
-          className='ml-12 w-full border border-black'
+            type='text'
+            id="category"
+            name="category"
+            value={product.category}
+            onChange={handleChange}
+            className='ml-12 w-full border border-black'
         />
-      </div>
-      
+    </div>
         <button type="submit" className=' mt-6 border border-black p-2' >Submit Changes</button>
-      
-      
     </form>
     </div>
-  )
+)
 }
 
 export default CreateProduct
--- frontend/e-commerce-shop/src/pages/Admin/Products.jsx
+++ frontend/e-commerce-shop/src/pages/Admin/Products.jsx
@@ -28,6 +28,28 @@ const Products = () => {
         fetchProduct();
       }, []);
 
+      const handleRemove = async (_id) => {
+        console.log(_id)
+        try {
+          const response = await fetch(api + '/admin/products', {
+            method: 'DELETE',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            credentials: 'include',
+            body: JSON.stringify({
+              productId: _id,
+            }),
+          });
+          const data = await response.json();
+    
+          // update the cart state after removing the item
+          setProducts(data);
+        } catch (err) {
+          console.log(err);
+        }
+      };
+
 return (
     <div className='mx-auto w-2/3'>
       <Link to='/create' className='text-xl border border-black p-2'>
@@ -45,7 +67,7 @@ return (
             <p className='grid content-center'>{product.price}€</p>
             <p className='grid content-center'>{product.category}</p>
             <Link to={`/edit-product/${product._id}`} className='border border-black w-28 h-10 text-center content-center grid'>Edit Product</Link>
-            <button className='border border-black w-32 h-10 text-center content-center grid'>Remove Product</button>
+            <button className='border border-black w-32 h-10 text-center content-center grid' onClick={()=> handleRemove(product._id)}>Remove Product</button>
       </div>
       ))}
     </div>
