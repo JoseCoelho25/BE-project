4bb72752b3caf53ea16b4c7b195c0d7ab4ae3136
JoseCoelho25 <zenani87@hotmail.com>
2023-03-25 15:40:15 +0000

    added history display on chat

--- frontend/e-commerce-shop/src/components/Chat.jsx
+++ frontend/e-commerce-shop/src/components/Chat.jsx
@@ -4,33 +4,46 @@ const socket = io.connect('http://localhost:5000')
 
 const Chat = () => {
   const [message, setMessage] = useState('')
+  const [messages, setMessages] = useState([])
   const [messageReceived, setMessageReceived] = useState('')
-  const sendMessage = () => {
-    // const message = document.querySelector('input').value
-    socket.emit('send_message', {message})
-  }
-    socket.on('message', (data) => {
-      console.log(data)
-     })
-    
-     useEffect(() => {
-        socket.on('receive_message', (data) => {
-          setMessageReceived(data.message)
-        })
-     }, [socket])
-    
-  return (
-    <div>
-        <h1>Chat</h1>
-    <div id="chat">
 
-    </div>
-    <input placeholder="Message..." onChange={(event) => {
-      setMessage(event.target.value)
-    }}/>
-    <button onClick={sendMessage}>Enviar mensagem</button>
-    <h1>Message:</h1>
-    {messageReceived}
+  const sendMessage = () => {
+    socket.emit('send_message', {message})
+    setMessages([...messages, {sender: 'me', message: message}])
+    setMessage('')
+  }
+
+  socket.on('message', (data) => {
+    console.log(data)
+  })
+
+  useEffect(() => {
+    socket.on('receive_message', (data) => {
+      setMessages([...messages, {sender: 'other', message: data.message}])
+      setMessageReceived(data.message)
+    })
+  }, [socket, messages])
+  
+
+  return (
+    <div className='fixed bottom-20 bg-gray-500 border-'>
+      <h1>Chat</h1>
+      <div id="chat">
+        {messages.map((msg, index) => (
+          <div key={index}>
+            <p>{msg.sender}: {msg.message}</p>
+          </div>
+        ))}
+      </div>
+      <input placeholder="Message..." value={message} onChange={(event) => {
+        setMessage(event.target.value)
+      }} onKeyPress={(event) => {
+        if (event.key === 'Enter') {
+        sendMessage()
+        }
+      }}/>
+
+      <button onClick={sendMessage}>Enviar mensagem</button>
     </div>
   )
 }
