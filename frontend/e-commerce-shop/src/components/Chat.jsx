c35f296e3a90124b2bd6c371fd517e20a95dc4f3
JoseCoelho25 <zenani87@hotmail.com>
2023-03-25 14:53:34 +0000

    send and receive message with socket.io config

--- frontend/e-commerce-shop/src/components/Chat.jsx
+++ frontend/e-commerce-shop/src/components/Chat.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import io from 'socket.io-client'
 const socket = io.connect('http://localhost:5000')
 
 const Chat = () => {
+  const [message, setMessage] = useState('')
+  const [messageReceived, setMessageReceived] = useState('')
   const sendMessage = () => {
     // const message = document.querySelector('input').value
-    // socket.emit('message', message)
+    socket.emit('send_message', {message})
   }
     socket.on('message', (data) => {
       console.log(data)
      })
     
+     useEffect(() => {
+        socket.on('receive_message', (data) => {
+          setMessageReceived(data.message)
+        })
+     }, [socket])
     
   return (
     <div>
@@ -18,8 +25,12 @@ const Chat = () => {
     <div id="chat">
 
     </div>
-    <input placeholder="Message..."/>
+    <input placeholder="Message..." onChange={(event) => {
+      setMessage(event.target.value)
+    }}/>
     <button onClick={sendMessage}>Enviar mensagem</button>
+    <h1>Message:</h1>
+    {messageReceived}
     </div>
   )
 }
