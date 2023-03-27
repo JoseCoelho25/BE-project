19db2b0b9dce719575e32c94bd83c6b8aa28c8bc
JoseCoelho25 <zenani87@hotmail.com>
2023-03-27 19:07:45 +0100

    updated style for checkout pages

--- frontend/e-commerce-shop/src/pages/Checkout/Cancel.jsx
+++ frontend/e-commerce-shop/src/pages/Checkout/Cancel.jsx
@@ -1,19 +1,22 @@
 import React from "react"; 
- 
+import { Link } from "react-router-dom";
+
 function Cancel() { 
   return ( 
-    <> 
-      <h4>Oops! Your payment has been cancelled.</h4> 
+    <div className="mx-auto w-full text-center"> 
+      <h2 className="text-2xl pt-8">Ups algo correu mal!</h2> 
+      <h4 className="text-xl my-6">O seu pagamento não foi realizado com sucesso!</h4> 
       <p> 
-        We appreciate your business! If you have any questions, please email us 
-        at 
-        <a href="mailto:orders@example.com">orders@example.com</a>. 
+        Se necessitar de ajuda pode contactar-nos no email <a href="" className="underline">shopeasy@email.com</a>
       </p> 
-      <div> 
-        <button> Go to Home page</button> 
-      </div> 
-    </> 
+      <div className="mt-8 border-black border w-48 mx-auto">
+        <Link to='/'>
+          Back to Home Page
+        </Link> 
+      </div>
+      
+  </div> 
   ); 
 } 
- 
+
 export default Cancel; 
--- frontend/e-commerce-shop/src/pages/Checkout/Success.jsx
+++ frontend/e-commerce-shop/src/pages/Checkout/Success.jsx
@@ -1,19 +1,15 @@
 import React from "react"; 
- 
+
 function Success() { 
   return ( 
-    <> 
-      <h2>Thanks for your order!</h2> 
-      <h4>Your payment is successful.</h4> 
+    <div className="mx-auto w-full text-center"> 
+      <h2 className="text-2xl pt-8">Obrigado pela sua compra!</h2> 
+      <h4 className="text-xl my-6">O seu pagamento foi realizado com sucesso</h4> 
       <p> 
-        We appreciate your business! If you have any questions, please email us 
-        at 
-        <a href="mailto:orders@example.com">orders@example.com</a>. 
-      </p> 
-      <div> 
-      </div> 
-    </> 
+        Obrigado pela sua preferência e o recibo da sua compra foi enviado para o seu Email.
+      </p>  
+    </div> 
   ); 
 } 
- 
+
 export default Success; 
